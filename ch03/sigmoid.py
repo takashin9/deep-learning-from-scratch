7c71604725b9fba10a136d217dc3062a56940206
takashin9 <takashi64256@gmail.com>
2024-07-23 16:08:18 +0000

    WIP ch03 neuralnet mnist before batch

--- ch03/sigmoid.py
+++ ch03/sigmoid.py
@@ -4,8 +4,9 @@ import matplotlib.pylab as plt
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+if __name__ == '__main__':
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = sigmoid(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
--- ch03/step_function.py
+++ ch03/step_function.py
@@ -4,8 +4,9 @@ import matplotlib.pylab as plt
 def step_function(x):
     return np.array(x > 0, dtype=np.int16)
 
-x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
+if __name__ == '__main__':
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_function(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
