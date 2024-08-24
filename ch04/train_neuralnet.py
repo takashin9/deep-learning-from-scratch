9caadd6033d60c25aa1cbb9105ed026e9bbe9a9a
takashin9 <takashi64256@gmail.com>
2024-08-24 08:06:31 +0000

    wip ch04 train_neuralnet

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -2,13 +2,36 @@ import sys, os
 sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
+from two_layer_net import TwoLayerNet
 
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
 
+train_loss_list = []
+
+iters_num = 10000
 train_size = x_train.shape[0]
-batch_size = 10
-batch_mask = np.random.choice(train_size, batch_size)
-x_batch = x_train[batch_mask]
-t_batch = t_train[batch_mask]
+batch_size = 100
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    # ミニバッチ取得
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # 勾配の計算
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    # パラメータ更新
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # 学習経過の記録
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    
 
 
