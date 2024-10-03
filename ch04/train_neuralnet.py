37914f82620b0e2313a0deeb115bc10cf67aeb17
takashin9 <takashi64256@gmail.com>
2024-10-03 19:16:07 +0900

    WIP ch04

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -7,11 +7,15 @@ from two_layer_net import TwoLayerNet
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+# 1エポック当たりの繰り返し数
+iter_per_epoch = max(train_size / batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -31,6 +35,14 @@ for i in range(iters_num):
     # 学習経過の記録
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
+
+    # 1エポックごとに認識精度を計算
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
 
     
 
